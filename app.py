6f25911f1021ff72f7e2f084328e28923e06cfa8
Vinay-data-code <vr8181880@gmail.com>
2026-06-04 21:32:14 +0530

    fix: removed pkl issue

--- app.py
+++ app.py
@@ -1,156 +1,164 @@
 import streamlit as st
-import pickle
 import pandas as pd
 import requests
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Load data
-movies_dict = pickle.load(open('movies.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
+# API
+import os
+API_KEY = os.getenv("73a0a5b2c907006e9510b0468c80ff95")
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+# Load dataset
+movies = pd.read_csv("tmdb_5000_movies.csv")
+credits = pd.read_csv("tmdb_5000_credits.csv")
 
-API_KEY = "73a0a5b2c907006e9510b0468c80ff95"
+movies = movies.merge(credits, on="title")
+
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
 
 # -------- FUNCTIONS -------- #
+
+def convert(text):
+    L = []
+    for i in ast.literal_eval(text):
+        L.append(i['name'])
+    return L
+
+def get_top_cast(text):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(text):
+        if counter < 3:
+            L.append(i['name'])
+            counter += 1
+        else:
+            break
+    return L
+
+def fetch_director(text):
+    L = []
+    for i in ast.literal_eval(text):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+    return L
+
+# Apply
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(get_top_cast)
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['overview'] = movies['overview'].fillna('').apply(lambda x: x.split())
+
+movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ","") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ","") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ","") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x: [i.replace(" ","") for i in x])
+
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+new_df = movies[['movie_id','title','tags']]
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x).lower())
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+# -------- API FUNCTIONS -------- #
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
     data = requests.get(url).json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
-def fetch_cast(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={API_KEY}"
-    data = requests.get(url).json()
-    
-    cast_names = []
-    cast_images = []
-    
-    for i in data['cast'][:5]:
-        cast_names.append(i['name'])
-        
-        if i['profile_path']:
-            cast_images.append("https://image.tmdb.org/t/p/w500/" + i['profile_path'])
-        else:
-            cast_images.append("https://via.placeholder.com/150")
-
-    return cast_names, cast_images
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = similarity[index]
-    
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
-    recommended_movies = []
-    recommended_posters = []
-
-    for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_posters.append(fetch_poster(movie_id))
-
-    return recommended_movies, recommended_posters
-
 def fetch_movie_details(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
     data = requests.get(url).json()
-    
-    poster = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-    title = data['title']
-    rating = data['vote_average']
-    overview = data['overview']
-    
-    return poster, title, rating, overview
+    return (
+        "https://image.tmdb.org/t/p/w500/" + data['poster_path'],
+        data['title'],
+        data['vote_average'],
+        data['overview']
+    )
 
+def fetch_cast(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={API_KEY}"
+    data = requests.get(url).json()
+
+    names = []
+    images = []
+
+    for i in data['cast'][:5]:
+        names.append(i['name'])
+        if i['profile_path']:
+            images.append("https://image.tmdb.org/t/p/w500/" + i['profile_path'])
+        else:
+            images.append("https://via.placeholder.com/150")
+
+    return names, images
+
+def recommend(movie):
+    index = new_df[new_df['title'] == movie].index[0]
+    distances = similarity[index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    names = []
+    posters = []
+
+    for i in movies_list:
+        movie_id = new_df.iloc[i[0]].movie_id
+        names.append(new_df.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
+
+    return names, posters
 
 # -------- UI -------- #
 
 st.title("🎬 Movie Recommender System")
 
-search_term = st.text_input("🔍 Search movie")
+search = st.text_input("Search Movie")
 
 selected_movie = None
 
-if search_term:
-    results = movies[movies['title'].str.contains(search_term, case=False, na=False)]
-    
-    if not results.empty:
-        selected_movie = st.selectbox("Select movie", results['title'].values)
-    else:
-        st.warning("No movie found")
+if search:
+    result = new_df[new_df['title'].str.contains(search, case=False)]
+    if not result.empty:
+        selected_movie = st.selectbox("Select Movie", result['title'])
 
+if st.button("Recommend") and selected_movie:
 
-# ✅ BUTTON CLICK KE ANDAR SAB AAYEGA
-if st.button('Recommend') and selected_movie:
+    movie_id = new_df[new_df['title'] == selected_movie].iloc[0].movie_id
 
-    # movie id
-    movie_id = movies[movies['title'] == selected_movie].iloc[0].movie_id
-
-    # -------- SELECTED MOVIE -------- #
     poster, title, rating, overview = fetch_movie_details(movie_id)
 
     st.subheader("🎬 Selected Movie")
-    col1, col2 = st.columns([1,2])
 
+    col1, col2 = st.columns([1,2])
     with col1:
         st.image(poster)
-
     with col2:
         st.markdown(f"**{title}**")
         st.markdown(f"⭐ Rating: {rating}")
-        st.markdown(f"{overview}")
+        st.write(overview)
 
-    # -------- CAST -------- #
     st.subheader("🎭 Cast")
+    names, images = fetch_cast(movie_id)
 
-    cast_names, cast_images = fetch_cast(movie_id)
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.image(images[i])
+            st.caption(names[i])
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+    st.subheader("🎯 Recommended")
 
-    with col1:
-        st.image(cast_images[0])
-        st.caption(cast_names[0])
+    rec_names, rec_posters = recommend(selected_movie)
 
-    with col2:
-        st.image(cast_images[1])
-        st.caption(cast_names[1])
-
-    with col3:
-        st.image(cast_images[2])
-        st.caption(cast_names[2])
-
-    with col4:
-        st.image(cast_images[3])
-        st.caption(cast_names[3])
-
-    with col5:
-        st.image(cast_images[4])
-        st.caption(cast_names[4])
-
-    # -------- RECOMMENDATIONS -------- #
-    st.subheader("🎯 Recommended Movies")
-
-    names, posters = recommend(selected_movie)
-
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-        st.image(posters[0])
-        st.caption(names[0])
-
-    with col2:
-        st.image(posters[1])
-        st.caption(names[1])
-
-    with col3:
-        st.image(posters[2])
-        st.caption(names[2])
-
-    with col4:
-        st.image(posters[3])
-        st.caption(names[3])
-
-    with col5:
-        st.image(posters[4])
-        st.caption(names[4])
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.image(rec_posters[i])
+            st.caption(rec_names[i])
